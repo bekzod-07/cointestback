1180b8c9c990960e4c71182b223f478f315c2679
bekzod-07 <bekzodsindorov0@gmail.com>
2025-12-30 12:20:04 +0500

    ddd

--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,7 @@ TEMPLATES = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "/home/bekzod/Desktop/cointestback/myproject/db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
